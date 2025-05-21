c4a92d76f323dd791ce755751f6f511da70b22f3
Gary Davies <GaryDaviesHome@Gmail.com>
2025-05-21 15:05:23 +0800

    AnimatedViewBox

--- app/components/AnimatedViewBox.tsx
+++ app/components/AnimatedViewBox.tsx
@@ -2,8 +2,11 @@ import { ReactElement } from "react";
 import Svg, { Circle, Rect } from "react-native-svg";
 
 const AnimatedViewBox = (props): ReactElement => {
+  // const viewBoxCoords = "-5 -5 10 10";
+  const viewBoxCoords = "0 0 100 100";
+
   return (
-    <Svg viewBox="-5 -5 10 10" xmlns="http://www.w3.org/2000/svg">
+    <Svg viewBox={viewBoxCoords} xmlns="http://www.w3.org/2000/svg">
       <Rect x="0" y="0" width="100%" height="100%" />
       <Circle cx="50%" cy="50%" r="4" fill="white" />
     </Svg>
