2d255703c0af3b066173b68979e8f34829e26e17
Gary Davies <GaryDaviesHome@Gmail.com>
2025-05-21 15:18:48 +0800

    AnimatedViewBox

--- app/components/AnimatedViewBox.tsx
+++ app/components/AnimatedViewBox.tsx
@@ -1,10 +1,22 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import Svg, { Circle, Rect } from "react-native-svg";
 
 const AnimatedViewBox = (props): ReactElement => {
   // const viewBoxCoords = "-5 -5 10 10";
-  const viewBoxCoords = "0 0 100 100";
+  // const viewBoxCoords = "0 0 100 100";
+  const [viewBoxCoords, setViewBoxCoords] = useState<string>("0 0 100 100");
+  const [toggle, setToggle] = useState<boolean>(false);
 
+  useEffect(() => {
+    const intervaleTimer = setInterval(() => {
+      setViewBoxCoords(toggle ? "0 0 100 100" : "-5 -5 10 10");
+      setToggle((toggle) => !toggle);
+    }, 500);
+
+    return () => {
+      clearInterval(intervaleTimer);
+    };
+  });
   return (
     <Svg viewBox={viewBoxCoords} xmlns="http://www.w3.org/2000/svg">
       <Rect x="0" y="0" width="100%" height="100%" />
